760c39dd32f18abff400ff08a7cc22ec570a51ed
mediatrice8 <kampiremediatrice@gmail.com>
2019-08-04 13:18:36 +0200

    i add the function to the js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ $(document).ready(function(){
         $("#show-contact").slideToggle("slow");
       });
   });
+
+  var pizza = (element) => {
+    return document.querySelector(element);
+  }
+  const getCheckOutData = ()=>{
+    pizza('[details]').style.display = 'block';
+    pizza('span.size').innerHTML = pizza('#quantitySize').value;
+    pizza('span.crust').innerHTML = pizza('#crust').value;
+    pizza('span.topping').innerHTML = pizza('#topping').value;
+    pizza('span.numbr').innerHTML = pizza('#numberSize').value;
+    pizza('span.total').innerHTML = pizza('#moneyToPay').value;
+  }
